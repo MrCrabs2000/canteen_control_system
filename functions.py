0c3141f2e306c7632f32c4f4bb69a985080d11aa
ReZeY007 <rozhkov.andr2009@gmail.com>
2026-01-12 19:49:53 +0300

    Добавлен вывод дат в сайдбаре страницы menu Добавлена функция для получения промежутка дат. Сделано добавление дат на страницу

--- functions.py
+++ functions.py
@@ -1,5 +1,5 @@
 from json import loads, dumps
-
+from datetime import date, timedelta
 
 
 def json_to_str(json_text):
@@ -19,3 +19,16 @@ def str_to_json(text):
     for i in range(len(text)):
         res[str(i)] = text[i]
     return dumps(res)
+
+
+def get_dates(delta_past, delta_future):
+    start = date.today() - timedelta(days=delta_past)
+    
+    dates = list()
+    prev_date = start
+
+    for i in range(delta_past + delta_future + 1):
+        dates.append(prev_date)
+        prev_date = prev_date + timedelta(days=1)
+
+    return dates
