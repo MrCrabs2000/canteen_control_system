3dc85715da36c90c7b3c1423c35abfb5a4464dae
AlexBit99 <Shapito969@gmail.com>
2026-01-17 17:45:07 +0300

    commit before pull

--- routes/menu.py
+++ routes/menu.py
@@ -2,24 +2,27 @@ from flask import Blueprint, render_template
 from flask_login import login_required
 from sqlalchemy.orm import joinedload
 from datetime import date
-
+from flask import request
 from datebase import db_session
 from datebase.classes import Menu, Dish
 from utils.templates_rendering.menu import render_menu_template
 
 
 menu_page = Blueprint('menu_page', __name__, template_folder='templates')
-@menu_page.route('/menu')
+@menu_page.route('/menu/<date>')
 @login_required
-def menupage():
-    session_db = db_session.create_session()
+def menupage(date):
 
-    menus = session_db.query(Menu).options(joinedload(Menu.dishes).joinedload(Dish.products)).all()
+    type = request.args.get('type')
+
+    session_db = db_session.create_session()
+    menu = session_db.query(Menu).options(joinedload(Menu.dishes).joinedload(Dish.products)).filter_by(date=date, type=type).first()
 
     context = {
-        'menus': menus,
+        'menu': menu,
     }
 
     session_db.close()
 
-    return render_template('menu/view.html', **context)
+    return render_menu_template(**context)
+
