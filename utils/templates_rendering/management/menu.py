58a85d7fb492ac0c3228345b20868e2cb7e73119
ReZeY007 <rozhkov.andr2009@gmail.com>
2026-01-30 00:11:25 +0300

    Набросано изменение меню

--- utils/templates_rendering/management/menu.py
+++ utils/templates_rendering/management/menu.py
@@ -1,17 +1,10 @@
-from flask import render_template
-
-
-def render_add_menu_template(dishes, name: str, surname: str, **kwargs):
-    context = {
-        'name': name,
-        'surname': surname,
-        'categories': {}
-    }
+def get_dishes_in_categories(dishes):
+    categories = {}
 
     for dish in dishes:
-        if dish.category not in context['categories']:
-            context['categories'][dish.category] = [(dish.name, dish.name)]
+        if dish.category not in categories:
+            categories[dish.category] = [(dish.name, dish.name)]
         else:
-            context['categories'][dish.category].append((dish.name, dish.name))
+            categories[dish.category].append((dish.name, dish.name))
 
-    return render_template('management/menus/add.html', **context, **kwargs)
+    return categories
